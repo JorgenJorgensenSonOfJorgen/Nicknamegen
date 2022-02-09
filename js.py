db3c4c3577bf560cc3e70ac2da16892d8ca29d4a
JorgenJorgensenSonOfJorgen <zhengbrayden@gmail.com>
2022-02-09 13:49:44 -0700

    Did it

--- js.py
+++ js.py
@@ -3,32 +3,22 @@
 
 import random
 
-nicknames = ['The Flash', 'The Cowboy', 'QiQi', 'The Addict', '?', 'The Bitter', 'The Indecisive', 'The Speler', 'White Wolf', '4782817', 'The Unlucky', 'The Chosen One', 'The Amnesiac', 'C', 'Happy Apple', 'Mysterious Melon']
+nicknames = ['The Flash', 'The Cowboy', 'QiQi', 'The Addict', '?', 'The Bitter', 'The Indecisive', 'The Speler', 'White Wolf', '4782817', 'The Unlucky', 'The Chosen One', 'The Amnesiac', 'Happy Apple', 'Mysterious Melon']
 
-#we create a nicknameMap of arrays where we assign each nickname to an index (I just wanted to try this out because its fast)
-
+#I want to create a dictionary because it is faster.
 nicknameMap = {}
 for i in range(len(nicknames)):
     nicknameMap[nicknames[i]] = i
+
 print('type "quit" to quit, "remove to remove a name, "add" to add a name, and "list" to see the full list of names')
 
-def Unlucky():
-    pass
-
-def cursed(i):
-    input("give your name or one of the commands ")
-    print('CURSED'[i])
-    if i ==  5:
-        i = 0
+def formatOutput(nameSplit,nickname):
+    if len(nameSplit) > 1:
+        print(nameSplit[0] + ' "' + nickname + '" ' + nameSplit[1])
     else:
-        i += 1
-    cursed(i)
-    pass
+        print(nameSplit[0] + ' "' + nickname + '"' )
 
-def Amnesia():
-    pass
-
-def repeat(variant):
+def repeat(variant,count):
     name = input("give your name or one of the commands ")
 
     if name == 'quit':
@@ -36,8 +26,7 @@ def repeat(variant):
     elif name == 'remove':
         removal = input('type a valid name to remove ')
 
-        if removal in nicknameMap: #this is fast
-            nicknames.pop(nicknameMap[removal])
+        if removal in nicknameMap:
             del nicknameMap[removal]
             print(removal + ' removed')
         else:
@@ -47,42 +36,43 @@ def repeat(variant):
         addition = input('type a nickname to add. duplicates not allowed! ')
 
         if addition not in nicknameMap:
-            nicknameMap[addition] = len(nicknames)
-            nicknames.append(addition)
+            nicknameMap[addition] = True
             print(addition + ' added')
         else:
             print("that nickname is already in the list!")
 
     elif name == 'list':
-        for i in nicknames:
+        for i in nicknameMap:
             print(i)
 
     else:
-        #ok we want to add the name of the person as an actual nickname possibility as well
-
-        nicknames.append(name)
-        name = name.split(' ', 1)
+        nameSplit = name.split(' ', 1)
 
         #after we are done this, we must implemenet AMNESIA
-
-        rand = random.random()
-        if rand < 0.0001:
-            print(name[0] + ' "1 in 10 thousand" ' + name[len(name) - 1])
+        if variant:
+            formatOutput(nameSplit, variant['func'])
+            count -=1
+            if count == 0:
+                variant = False
         else:
-
-            rand = nicknames[random.randint(0,len(nicknames) - 1)]
-            if len(name) > 1:
-                print(name[0] + ' "' + rand + '" ' + name[len(name) - 1])
+            if random.random() < 0.0001:
+                formatOutput(nameSplit, 'one in 10 thousand')
             else:
-                print(name[0] + ' "' + rand + '" ' )
-        nicknames.pop()
-
-        if rand == 'C':
-            cursed(1)
-        elif rand in specialcon:
-            pass
-    repeat(variant)
+                #ok we want to add the name of the person as an actual nickname possibility as well
+                if name not in nicknameMap:
+                    nicknameMap[name] = True
+                    nickname = random.choice(list(nicknameMap.keys()))
+                    formatOutput(nameSplit, nickname)
+                    del nicknameMap[name]
+                else:
+                    nickname = random.choice(list(nicknameMap.keys()))
+                    formatOutput(nameSplit, nickname)
+            if nickname in specialcon:
+                variant = specialcon[nickname]
+                count = specialcon[nickname]['count']
+    repeat(variant,count)
 
 #these are special conditions which when the name is gotten, I want to have special recurring effects
-specialcon = {'Amnesiac' : {'func':Amnesia, 'count': 0}, 'Unlucky' : {'func':Unlucky, 'count': 0}}
-repeat()
+specialcon = {'The Amnesiac' : {'func':'The Amnesiac', 'count': 5
+}, 'The Unlucky' : {'func':'crap', 'count': 5}}
+repeat(False, 0)
